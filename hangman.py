4254b860b9fcbd9b1028c6f74da53ffcbb4c834d
NadjaPN <pavlova.nadezsda@gmail.com>
2021-12-21 13:37:57 +0100

    Step 5.

--- hangman.py
+++ hangman.py
@@ -28,10 +28,10 @@ def lives(choosing_int):
         lives = 15
             #import smaller word
     elif choosing_int == 2:
-        lives = 13
+        lives = 10
             #import longer word
     else:
-        lives = 11
+        lives = 5
             #import longest word
     return lives
 
@@ -48,7 +48,7 @@ def country_or_capital():
         elif country_or_capital == 'CPT':
             return 1
         else:
-            print("Choose from 'CNT' or 'CPT'")
+            country_or_capital = input("Choose from 'CNT' or 'CPT'")
 
 CNT_or_CPT = country_or_capital()
 
@@ -71,29 +71,28 @@ def choosen_word (CNT_or_CPT, word_line):
     else:
         return word_line_split[1]
 
-word = choosen_word(CNT_or_CPT,word_line)
-print(f"The choosen word is {word}") 
+word_to_guess = choosen_word(CNT_or_CPT,word_line)
+print(f"The choosen word is {word_to_guess}") 
 
 
 # STEP 2
 # based on the chosen difficulty level, set the values 
 # for the player's lives
-word_to_guess = "Cairo" # sample data, normally the word should be chosen from the countries-and-capitals.txt
-lives = 5 # sample data, normally the lives should be chosen based on the difficulty
-
-
+# word_to_guess = "Cairo" # sample data, normally the word should be chosen from the countries-and-capitals.txt
+# lives = 5 # sample data, normally the lives should be chosen based on the difficulty
 
 
 # STEP 3
 # display the chosen word to guess with all letters replaced by "_"
 # for example instead of "Cairo" display "_ _ _ _ _"
 
-def play(word_to_guess):
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = lives
+def display(word_to_guess):
+    word_completion = "_ " * (len(word_to_guess)-1)
+    return word_completion
+
+secret_word = display(word_to_guess)
+print("Your word to guess:")
+print(secret_word)
 
 
 # STEP 4
@@ -102,8 +101,6 @@ def play(word_to_guess):
 # "quit", "Quit", "QUit", "QUIt", "QUIT", "QuIT"... you get the idea :)
 # HINT: use the upper() or lower() built-in Python functions
 
-def guess(letter):
-    input("Please guess a letter or word: ").upper()
 
 # STEP 5
 # validate if the typed letter is already in the tried letters
@@ -112,11 +109,24 @@ def guess(letter):
 # If it has already been typed, return to STEP 5. HINT: use a while loop here
 already_tried_letters = [] # this list will contain all the tried letters
 
+def validate(already_tried_letters):
+    guess_letter = input("Please guess a letter or word: ").upper()
+    while True:
+        if guess_letter in already_tried_letters:
+            print("You already guessed this letter!")
+            guess_letter = input("Try an other one: ")
+        else:
+            already_tried_letters.append(guess_letter)
+            return guess_letter
+
+guess = validate(already_tried_letters)
+print(guess)
 
 # STEP 6
 # if the letter is present in the word iterate through all the letters in the variable
 # word_to_guess. If that letter is present in the already_tried_letters then display it,
 # otherwise display "_".
+
 
 
 # if the letter is not present in the word decrease the value in the lives variable
